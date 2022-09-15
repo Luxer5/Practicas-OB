daa5b9de97c050795c56f35778e9b172c07cb555
Dani <danielcalsanz@gmail.com>
2022-09-15 04:32:13 -0700

    Ej T1 java terminado

---
+++ EjericioJava1.java
@@ -0,0 +1,35 @@
+
+public class EjericioJava1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		byte variable1 = 10;
+		short variable2 = 2;
+		int variable3= 12;
+		long variable4 = 250;
+		
+		System.out.println("Impresion de los diferentes tipos de datos numericos introducidos "+
+				variable1+ "  "+ variable2 + "  "+ variable3+ "  " +  variable4);
+		
+		float decimal = 2.25f;
+		double decimal2 = 25.5d;
+		
+		System.out.println("Impresion de los diferentes tipos de datos decimales introducidos "+
+		decimal+ "  " + decimal2);
+		
+		boolean booleano1 = true;
+		boolean booleano2 = false;
+		
+		System.out.println("Impresion de los diferentes tipos de valores booleanos "+
+		booleano1+ "  "+ booleano2);
+		
+		char carac= '5';
+		String frase = "primer string";
+
+		System.out.println("Impresion de los diferentes tipos de vadenas de caracteres "+
+				carac+ "  " + frase);
+		
+	}
+
+}
