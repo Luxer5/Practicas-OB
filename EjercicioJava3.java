d01f58783323152550bbc83c79ca5002e3277184
Dani <danielcalsanz@gmail.com>
2022-09-19 03:36:43 -0700

    Ejercicio T3 completo

---
+++ EjercicioJava3.java
@@ -0,0 +1,19 @@
+
+public class EjercicioJava3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String texto="";
+
+		String[] palabras = {"Hoy","es", "viernes."};
+		
+		for (String palabra :  palabras) {
+			texto += " "+palabra;
+			
+		}
+		System.out.print(texto);
+		
+		
+	}
+
+}
