d18bc3075e770ac75a8471de8b66f20e9968d137
Dani <danielcalsanz@gmail.com>
2022-09-19 23:52:36 -0700

    Cambio Main

--- EjercicioJava4/Main.java
+++ EjercicioJava4/Main.java
@@ -6,15 +6,15 @@ public class Main {
 		// TODO Auto-generated method stub
 
 		SmartPhone galaxy22= new SmartPhone(5000, "Samsung", 6.7, "Galaxy 22");
-		SmartWatch watch5= new SmartWatch(500, "Huaweii", 22.5, "Watch 5");
+		SmartWatch watch5= new SmartWatch(500, "Huawei", 22.5, "Watch 5");
 		
-		System.out.println("Modelo movil: " + galaxy22.bateria);
-		System.out.println("Marca: "+ galaxy22.marca);
+		System.out.println("Marca movil: "+ galaxy22.marca);
+		System.out.println("Bateria: " + galaxy22.bateria);
 		System.out.println("Pantalla: "+galaxy22.tamPantalla);
 		System.out.println("Modelo: "+galaxy22.modelo);
 		
-		System.out.println("Modelo reloj: " + watch5.bateria);
-		System.out.println("Marca: "+ watch5.marca);
+		System.out.println("Marca reloj: " + watch5.marca);
+		System.out.println("Bateria: "+ watch5.bateria);
 		System.out.println("Correa: "+watch5.tamCorrea);
 		System.out.println("Modelo: "+watch5.modelo);
 		
